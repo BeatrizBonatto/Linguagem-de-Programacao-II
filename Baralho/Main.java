26315ee24a9941018f373d84f350b8e9de69b4c3
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2024-03-01 15:02:59 -0300

    🚧 : Classe Main em progresso

--- Baralho/Main.java
+++ Baralho/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args){
         Baralho baralho = new Baralho();
+        baralho.embaralhar();
+        baralho.hasCarta();
+        //baralho.distribuirCarta();
+        baralho.imprimirBaralho();
         
-        System.out.println(baralho);
-        
+        //System.out.println(baralho.hasCarta());  - testando true false
     }
 }
