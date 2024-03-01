ae4a7d3430fa5304f8eaabdf357b596ba43b1207
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2024-03-01 15:05:13 -0300

    ♻️ refactor: Arrumando valor de enum
    Arrumando "_K" para "c_K" por padrão

--- Baralho/Nome.java
+++ Baralho/Nome.java
@@ -1,3 +1,3 @@
 public enum Nome {
-        c_A, c_2, c_3, c_4, c_5, c_6, c_7, c_8, c_9, c_10, c_J, c_Q, _K, Coringa;
+        c_A, c_2, c_3, c_4, c_5, c_6, c_7, c_8, c_9, c_10, c_J, c_Q, c_K, Coringa;
 }
