6ca29b9f7c880d6915b6511de92f4131777da758
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2024-05-21 14:02:47 -0300

    Update Contatinho.java

--- edu.fatec.lp2.exercicio1/Contatinho.java
+++ edu.fatec.lp2.exercicio1/Contatinho.java
@@ -1,3 +1,25 @@
 public class Contatinho {
-    
+    private String nome;
+    private String celular;
+
+    public Contatinho(String celular, String nome) {
+        this.celular = celular;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
 }
